92f9ed241adac71d4381792722aa64c0f53c1284
Till Klocke <till.klocke@gmail.com>
2017-07-25 10:33:50 +0200

    Removed CFLAGS which can cause errors on some gcc versions

--- src/modules/rlm_go/all.mk
+++ src/modules/rlm_go/all.mk
@@ -15,8 +15,10 @@ TARGET = $(PACKAGE).a
 # Very dirty hack, to prevent ar from trying to link stuff together. cgo has already taken care of that
 TGT_LINKER := "echo"
 
-CGO_FLAGS1 := $(subst -I,-I${top_srcdir}/,$(CFLAGS))
-CGO_CFLAGS := $(subst -include ,-include ${top_srcdir}/, $(CGO_FLAGS1))
+CGO_CFLAGS1 := $(subst -I,-I${top_srcdir}/,$(CFLAGS))
+CGO_CFLAGS2 := $(subst -include ,-include ${top_srcdir}/, $(CGO_CFLAGS1))
+# This is probably really hacky, but cgo seems to generate code causing errors
+CGO_CFLAGS := $(subst -Werror=strict-prototypes,,$(CGO_CFLAGS2))
 
 CGO_LDFLAGS := -L${top_srcdir}/build/lib/.libs/ -lfreeradius-server -lfreeradius-radius
 CGO_LDFLAGS += $(LDFLAGS)
