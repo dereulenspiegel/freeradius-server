92637cdaed84f1c46cf1782f2d8baddb305f2d64
Till Klocke <till.klocke@gmail.com>
2017-07-25 10:22:29 +0200

    We are now using the real CFLAGS and dynamically convert the incude paths to absolute paths

--- src/modules/rlm_go/all.mk
+++ src/modules/rlm_go/all.mk
@@ -15,19 +15,8 @@ TARGET = $(PACKAGE).a
 # Very dirty hack, to prevent ar from trying to link stuff together. cgo has already taken care of that
 TGT_LINKER := "echo"
 
-# TODO somehow use regex to add ${top_srcdir} before every include path
-CGO_CFLAGS := -I. -I${top_srcdir}/src \
-	-include ${top_srcdir}/src/freeradius-devel/autoconf.h \
-	-include ${top_srcdir}/src/freeradius-devel/build.h \
-	-include ${top_srcdir}/src/freeradius-devel/features.h \
-	-include ${top_srcdir}/src/freeradius-devel/radpaths.h \
-	-fno-strict-aliasing -g3 -Wall -std=c99 -D_GNU_SOURCE \
-	-D_REENTRANT -D_POSIX_PTHREAD_SEMANTICS -DDARWIN -DOPENSSL_NO_KRB5 \
-	-Wdocumentation -Wshadow -Wpointer-arith -Wcast-qual \
-	-Wcast-align -Wwrite-strings -Wstrict-prototypes -Wmissing-prototypes \
-	-Wmissing-declarations -Wnested-externs -W -Wredundant-decls -Wundef \
-	-Wformat-y2k -Wno-format-extra-args -Wno-format-zero-length -Wno-cast-align \
-	-Wformat-nonliteral -Wformat-security -Wformat=2 -DWITH_VERIFY_PTR=1 -DIS_MODULE=1
+CGO_FLAGS1 := $(subst -I,-I${top_srcdir}/,$(CFLAGS))
+CGO_CFLAGS := $(subst -include ,-include ${top_srcdir}/, $(CGO_FLAGS1))
 
 CGO_LDFLAGS := -L${top_srcdir}/build/lib/.libs/ -lfreeradius-server -lfreeradius-radius
 CGO_LDFLAGS += $(LDFLAGS)
@@ -39,6 +28,7 @@ go_build_static: $(BASE) go_build_dynamic
 	-o $(top_builddir)/$(BUILD_DIR)/lib/local/$(PACKAGE).a ./
 
 go_build_dynamic: $(BASE)
+	@echo "CGO_FLAGS $(CGO_CFLAGS)"
 	cd $(BASE) && \
 	GOPATH='$(LOCAL_GOPATH)' CGO_CFLAGS='$(CGO_CFLAGS)' CGO_LDFLAGS='$(CGO_LDFLAGS)' \
 	go build -buildmode=c-shared \
